10a90fc8826c1f788d294f93493b1d81a4986158
abdallah <abdallahkokash@outlook.com>
2025-10-04 22:47:51 +0300

    UI

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home');
+
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000 Test');
+app.listen(3000, async () => {
+    console.log('Server is running on port 3000');
 });
